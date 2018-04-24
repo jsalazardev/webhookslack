97848d76b9c91b8096b2a63b9c6a5ef1fef393e3
Jesus Salazar <jsalazardev@gmail.com>
2018-04-24 10:06:15 -0500

    Se agrega archivo conf y parametro username

--- README.md
+++ README.md
@@ -1,26 +1,38 @@
-# notificationSlack
-Notificaciones en web hook slack
+------------------------
+    NotificationSlack
+------------------------
 
-#install library
+- Install library
+
 python -m pip install requests --user --no-warn-script-location
 python -m pip install fire --user --no-warn-script-location
+python -m pip install configparser --user --no-warn-script-location
 
-#add permission run after clone repository
+- Add permission run after clone repository
+
 chmod +x notification.py
 
-#Modo de uso
-Despues de otorgar permisos para ejecucion, los parametros que reciben son:
-    text    -Requerido
-    title   -Opcional
-    footer  -Opcional
-    status  -Opcional   ['good' *valor por default, 'warning', 'danger', '<color en hex>']
+------------------------
+    Modo de uso
+------------------------
 
-Funciones deplegadas
+- Una vez otorgando los permisos para ejecucion, los parametros que reciben son:
+    text        - Requerido
+    title       - Opcional
+    footer      - Opcional
+    status      - Opcional
+    username    - Opcional
+
+    Posibles valores:
+    status      -'good'(valor por default*), 'warning', 'danger', '<color en hex>'
+    username    - tiene valores agregados por default de acuerdo a la petición
+
+-Funciones deplegadas
     deploy
     pull-request
     notification
 
-Invocación de las funciones
+-Invocación de las funciones
 
     ./notification.py --text="El despligue de en las instancias de produccion se ha realizado con exito" --footer="WAR 2.6.12" deploy
     ./notification.py --text="Ocurrio un problema con el ultimo WAR no inicia session" --footer="WAR 2.6.12" --status="danger" deploy
@@ -29,5 +41,6 @@ Invocación de las funciones
     ./notification.py --text="El pull request tiene unos detalles que correguir antes de confirmar"--footer="feature-payments -> dev" --status="warning" pull-request
 
     ./notification.py --text="Este solo es un mensaje para el grupo" notification
+    ./notification.py --text="Este solo es un mensaje para el grupo" --username="Terminal Unix" notification
 
 
--- notification.py
+++ notification.py
@@ -3,20 +3,23 @@
 import fire
 from module.webHook import send_message_to_slack
 
+
 class Notification(object):
 
-    def __init__(self, text, footer='', title='', status='good'):
-        self._text=text
-        self._footer=footer
-        self._title=title
-        self._status=status
+    def __init__(self, text, footer='', title='', status='good', username=''):
+        self._text = text
+        self._footer = footer
+        self._title = title
+        self._status = status
+        self._username = username
 
     def deploy(self):
-        params={
-            'text':self._text,
-            'title':'Notification Deploy' if self._title == '' else self._title,
-            'footer':'Instance {footer} '.format(footer='dev' if self._footer == '' else self._footer),
-            'status':self._status
+        params = {
+            'text': self._text,
+            'title': 'Notification Deploy' if self._title == '' else self._title,
+            'footer': 'Instance {footer} '.format(footer='dev' if self._footer == '' else self._footer),
+            'status': self._status,
+            'username': 'Amazon EC2' if self._username == '' else self._username
         }
         send_message_to_slack(**params)
 
@@ -25,7 +28,8 @@ class Notification(object):
             'text': self._text,
             'title': 'Notification Pull-Request' if self._title == '' else self._title,
             'footer': 'Merge {footer} '.format(footer='dev' if self._footer == '' else self._footer),
-            'status': self._status
+            'status': self._status,
+            'username': 'Amazon CodeCommit' if self._username == '' else self._username
         }
         send_message_to_slack(**params)
 
@@ -34,9 +38,11 @@ class Notification(object):
             'text': self._text,
             'title': 'Notification ' if self._title == '' else self._title,
             'footer': '{footer} '.format(footer='' if self._footer == '' else self._footer),
-            'status': self._status
+            'status': self._status,
+            'username': 'Application' if self._username == '' else self._username
         }
         send_message_to_slack(**params)
 
+
 if __name__ == '__main__':
-  fire.Fire(Notification)
+    fire.Fire(Notification)
